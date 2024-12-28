9dfef69c5e79ee072b2b09ec1465a86af2e07cc5
DoNcha21 <alx.donchenko@gmail.com>
2024-12-28 13:36:48 +0200

    remove main in lesson2

--- HW_1.1/src/main/java/org/example/lesson2/Main.java
+++ HW_1.1/src/main/java/org/example/lesson2/Main.java
@@ -1,25 +1,5 @@
 package org.example.lesson2;
 //dummy lesson2
 public class Main {
-    private static final double CONV_K = 1.60934;
-
-    public static void main(String[] args) {
-
-        double km = 1;
-        double miles = 12;
-
-        double milesResult = convKmToMiles(km);
-        double kmResult = convMilesToKm(miles);
-
-        System.out.println("Result is " + milesResult + " miles and " + kmResult + " kilometrs.");
-    }
-
-    private static double convKmToMiles(double km) {
-        return km / CONV_K;
-    }
-
-    private static double convMilesToKm(double miles) {
-        return miles * CONV_K;
-    }
 
 }
