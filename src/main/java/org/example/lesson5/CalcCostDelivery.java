e4d4ab49885fc7f3c246a39e0519c3d568eb0778
DoNcha21 <alx.donchenko@gmail.com>
2025-01-07 09:28:49 +0200

    5HW

---
+++ src/main/java/org/example/lesson5/CalcCostDelivery.java
@@ -0,0 +1,20 @@
+package org.example.lesson5;
+
+
+// Клас розрахунку вартості товару,
+// враховуючи вартість доставки
+// ЗАВДАННЯ: Виправити код класу.
+// Клас успадковує клас CalcCostBase.
+public class CalcCostDelivery extends CalcCostBase {
+
+    // Вартість доставки
+    private final static double deliveryPrice = 100;
+
+    // Розрахунок вартості товару,
+    // з урахуванням вартості доставки
+    @Override
+    public double calcCost(Product product) {
+        return product.getQuota() * product.getPrice()
+                + deliveryPrice;
+    }
+}
