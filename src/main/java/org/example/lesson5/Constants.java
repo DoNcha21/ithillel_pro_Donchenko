dedd51670272016e602a9b6fc4663b2f1d7184cc
DoNcha21 <alx.donchenko@gmail.com>
2025-01-08 20:35:54 +0200

    Dummy commit

--- src/main/java/org/example/lesson5/Constants.java
+++ src/main/java/org/example/lesson5/Constants.java
@@ -1,6 +1,6 @@
 package org.example.lesson5;
 
-public final class Constants {
+public final class  Constants {
 
     public final static String CURRENCY = "EUR";
     public final static String MEASURE = "pcs.";
