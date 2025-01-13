f6272d70049e2daf1be0d6cc3ade3f4868d1eacd
DoNcha21 <alx.donchenko@gmail.com>
2025-01-13 20:28:21 +0200

    7th hw - done

--- src/main/java/org/example/lesson4/Main.java
+++ src/main/java/org/example/lesson4/Main.java
@@ -1,3 +1,17 @@
+//
+//
+//class Custumer {
+//    private final String  name = "ddsdf";
+//
+//
+//}
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        System.out.println(new Custumer(name));
+//    }
+//}
 
 //
 //public class Custumer {
